a3c74d59bcf2f50d715d4ffff554ecf0b8646cb8
Alfiya <al.yausheva@gmail.com>
2025-08-03 13:52:26 +0300

    ⊗jsnxPmBsSC

--- src/app/test/page.jsx
+++ src/app/test/page.jsx
@@ -1,12 +1,34 @@
-export default function Test() {
-	let prods = [
-	'prod1',
-	'prod2',
-	'prod3',];
+'use client'
+import { React, useState } from "react";
 
-	for (let i=0; i<prods.length; i ++){
-		return <ul>{prods}</ul>
-	}
+export default function Test() {
+
+const [firstInput, setFirstInput] = useState(null);
+const [secondInput,setSecondInput] = useState(null);
+const [res,setRes] = useState(null);
+
+const handleInput1 = (e) => {
+setFirstInput(e.target.value)
+}
+const handleInput2 = (e) => {
+setSecondInput(e.target.value)
+}	
+
+const handleClick = () => {
+
+setRes(Number(firstInput)+Number(secondInput))
+
+}
+
+
+
+		return <>
+		<input type="number" value={firstInput} onChange={handleInput1} style={{backgroundColor:'white', border: "1px solid"}}></input>
+		 <input type="number" value={secondInput} onChange={handleInput2}style={{backgroundColor:'white', border: "1px solid"}}></input>		
+	<button style={{backgroundColor:'red', border: "3px solid"}} onClick={handleClick}>btn</button>
+	<p style={{backgroundColor:'grey', border: "1px solid"}}>{res}</p>
+		</>
+	
 
 
 }
