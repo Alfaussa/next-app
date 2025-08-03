ff9739f7779f6064aafa8b06632287e288410a51
Alfiya <al.yausheva@gmail.com>
2025-08-03 21:49:01 +0300

    ⊗jsnxPmRtSC

--- src/app/test/page.jsx
+++ src/app/test/page.jsx
@@ -1,34 +1,4 @@
-'use client'
-import { React, useState } from "react";
-
 export default function Test() {
-
-const [firstInput, setFirstInput] = useState(null);
-const [secondInput,setSecondInput] = useState(null);
-const [res,setRes] = useState(null);
-
-const handleInput1 = (e) => {
-setFirstInput(e.target.value)
-}
-const handleInput2 = (e) => {
-setSecondInput(e.target.value)
-}	
-
-const handleClick = () => {
-
-setRes(Number(firstInput)+Number(secondInput))
-
-}
-
-
-
-		return <>
-		<input type="number" value={firstInput} onChange={handleInput1} style={{backgroundColor:'white', border: "1px solid"}}></input>
-		 <input type="number" value={secondInput} onChange={handleInput2}style={{backgroundColor:'white', border: "1px solid"}}></input>		
-	<button style={{backgroundColor:'red', border: "3px solid"}} onClick={handleClick}>btn</button>
-	<p style={{backgroundColor:'grey', border: "1px solid"}}>{res}</p>
-		</>
-	
-
-
+	let name = 'user';
+	return <h1>hello, {name}!</h1>;
 }
