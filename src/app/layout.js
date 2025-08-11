eba1b87299e6795ab893525d35626069c3f01cad
Alfiya <al.yausheva@gmail.com>
2025-08-11 16:11:53 +0300

    ⊗jsnxPmLtCt

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,9 +7,13 @@ export default function RootLayout({children}) {
 	return (
 		<html lang="en">
 			<body>
+				<header>header</header>
 		<main>
 					{children}
+				<aside>aside-left</aside>
+				<aside>aside-right</aside>
 				</main>
+				<footer>footer</footer>
 			</body>
 		</html>
 	);
