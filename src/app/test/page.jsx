f46b007a38c537afeb4d1cdc7e13b97a686c6c04
Alfiya <al.yausheva@gmail.com>
2025-08-15 17:54:33 +0300

    ⊗jsnxPmStIC

--- src/app/test/page.jsx
+++ src/app/test/page.jsx
@@ -5,13 +5,16 @@ export const metadata = {
 	title: 'My Test page',
 	description: 'Description for test page.',
 }
-
+const textStyle ={
+	color: "purple",
+	fontSize: "100px"
+}
 export default function Test() {
 	
 
 		return  <>
 
-<p className={styles.text}>text text text</p>
+<p style={textStyle}>text text text</p>
 
 	 </>
 	}
