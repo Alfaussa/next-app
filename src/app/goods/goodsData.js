2b8cec78e02fc898ff1c694a4112f4d406312e57
Alfiya <al.yausheva@gmail.com>
2025-08-17 22:28:22 +0300

    ⊗jsnxPmPrjSC

---
+++ src/app/goods/goodsData.js
@@ -0,0 +1,184 @@
+const products = [
+  {
+    id: 1,
+    name: "Беспроводные наушники Xiaomi",
+    price: 2499,
+    image: "https://example.com/images/product1.jpg"
+  },
+  {
+    id: 2,
+    name: "Игровая мышь Logitech G502",
+    price: 4299,
+    image: "https://example.com/images/product2.jpg"
+  },
+  {
+    id: 3,
+    name: "Смарт-часы Amazfit Bip 3",
+    price: 3990,
+    image: "https://example.com/images/product3.jpg"
+  },
+  {
+    id: 4,
+    name: "Портативная колонка JBL GO 3",
+    price: 3690,
+    image: "https://example.com/images/product4.jpg"
+  },
+  {
+    id: 5,
+    name: "Внешний аккумулятор PowerBank 10000mAh",
+    price: 1490,
+    image: "https://example.com/images/product5.jpg"
+  },
+  {
+    id: 6,
+    name: "Механическая клавиатура Redragon",
+    price: 5590,
+    image: "https://example.com/images/product6.jpg"
+  },
+  {
+    id: 7,
+    name: "Монитор LG 27'' FullHD",
+    price: 16990,
+    image: "https://example.com/images/product7.jpg"
+  },
+  {
+    id: 8,
+    name: "Смартфон Samsung Galaxy A14",
+    price: 15990,
+    image: "https://example.com/images/product8.jpg"
+  },
+  {
+    id: 9,
+    name: "Планшет Lenovo Tab M10",
+    price: 11990,
+    image: "https://example.com/images/product9.jpg"
+  },
+  {
+    id: 10,
+    name: "Фитнес-браслет Xiaomi Mi Band 7",
+    price: 3290,
+    image: "https://example.com/images/product10.jpg"
+  },
+  {
+    id: 11,
+    name: "Bluetooth колонка Sony SRS-XB13",
+    price: 4990,
+    image: "https://example.com/images/product11.jpg"
+  },
+  {
+    id: 12,
+    name: "SSD накопитель Kingston 500GB",
+    price: 3990,
+    image: "https://example.com/images/product12.jpg"
+  },
+  {
+    id: 13,
+    name: "USB флешка SanDisk 64GB",
+    price: 799,
+    image: "https://example.com/images/product13.jpg"
+  },
+  {
+    id: 14,
+    name: "Умная лампочка Xiaomi Yeelight",
+    price: 1290,
+    image: "https://example.com/images/product14.jpg"
+  },
+  {
+    id: 15,
+    name: "Игровая гарнитура HyperX Cloud II",
+    price: 7990,
+    image: "https://example.com/images/product15.jpg"
+  },
+  {
+    id: 16,
+    name: "Мышь беспроводная Logitech M185",
+    price: 1090,
+    image: "https://example.com/images/product16.jpg"
+  },
+  {
+    id: 17,
+    name: "Геймпад Xbox Series Controller",
+    price: 5990,
+    image: "https://example.com/images/product17.jpg"
+  },
+  {
+    id: 18,
+    name: "Термокружка Xiaomi",
+    price: 890,
+    image: "https://example.com/images/product18.jpg"
+  },
+  {
+    id: 19,
+    name: "Настольная лампа Baseus",
+    price: 1490,
+    image: "https://example.com/images/product19.jpg"
+  },
+  {
+    id: 20,
+    name: "Коврик для мыши RGB",
+    price: 1390,
+    image: "https://example.com/images/product20.jpg"
+  },
+  {
+    id: 21,
+    name: "Наушники проводные KZ ZSN",
+    price: 2590,
+    image: "https://example.com/images/product21.jpg"
+  },
+  {
+    id: 22,
+    name: "Сетевой адаптер Anker 20W",
+    price: 1690,
+    image: "https://example.com/images/product22.jpg"
+  },
+  {
+    id: 23,
+    name: "Универсальный кабель USB-C",
+    price: 690,
+    image: "https://example.com/images/product23.jpg"
+  },
+  {
+    id: 24,
+    name: "Wi-Fi роутер TP-Link Archer",
+    price: 2990,
+    image: "https://example.com/images/product24.jpg"
+  },
+  {
+    id: 25,
+    name: "Веб-камера Logitech C920",
+    price: 7290,
+    image: "https://example.com/images/product25.jpg"
+  },
+  {
+    id: 26,
+    name: "Твердотельный накопитель Samsung 1TB",
+    price: 8990,
+    image: "https://example.com/images/product26.jpg"
+  },
+  {
+    id: 27,
+    name: "Гарнитура для работы Jabra Evolve 20",
+    price: 3990,
+    image: "https://example.com/images/product27.jpg"
+  },
+  {
+    id: 28,
+    name: "Док-станция Baseus USB-C",
+    price: 2590,
+    image: "https://example.com/images/product28.jpg"
+  },
+  {
+    id: 29,
+    name: "Карта памяти Samsung microSD 128GB",
+    price: 1790,
+    image: "https://example.com/images/product29.jpg"
+  },
+  {
+    id: 30,
+    name: "Зарядное устройство Xiaomi 33W",
+    price: 1390,
+    image: "https://example.com/images/product30.jpg"
+  }
+];
+
+export default products;
