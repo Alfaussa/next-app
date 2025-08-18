61b3b72d4007437916d4f58b847283faf1a37b60
Alfiya <al.yausheva@gmail.com>
2025-08-18 22:57:11 +0300

    styling with tailwind - homepage

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,23 +1,31 @@
-import Image from "next/image";
-import products from "./goods/goodsData"
 
+import products from "./goods/goodsData"
+import Link from "next/link";
 
 
 export default function Home() {
 
- let list = products.map((product) =>
-  (
-    <div>
-    <div>{product.name}</div>
-    <div>{product.price}</div>
-    <div>{product.image}</div>
-    </div>
-  )
-  )
+ const list = products.map((product) =>(
+<div className="flex w-50 h-100% border rounded-lg">
+  <li className="p-5" key={product.id}>
+  <Link href={`/goods/${product.id}`}>
+  
+  <h2 className="font-black text-center">{product.name}</h2>
+  <p  className="mt-2 mb-2  ">Цена: {product.price}</p>
+  <img className="size-50 shadow-xl rounded-md"src={product.image}></img>
+  
+  </Link>
+  </li>
+</div>
+ ))
+  
 
   return (<>
 
-{list}
+<div className="flex " >
+  <ul className="flex gap-4 flex-wrap ">{list}</ul>
+
+  </div>
 
 </>
   )
